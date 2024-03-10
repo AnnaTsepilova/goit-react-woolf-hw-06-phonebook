056a9aaeec9d261fb9141e5168a54d8f6ca30878
Anna Tsepilova <virgo.tsav@gmail.com>
2024-03-10 15:34:11 +0200

    refactored with redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,93 +1,150 @@
-import React, { useEffect, useState } from 'react';
-import { nanoid } from 'nanoid';
+import React, { useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import css from './App.module.css';
-import cssText from '../ContactsList/ContactsList.module.css';
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactsList from 'components/ContactsList/ContactsList';
 import Section from 'components/Section/Section';
 import Filter from 'components/Filter/Filter';
 
-import initialContacts from '../../data/contacts.json';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, deleteContact } from '../../redux/contactsSlice';
+import { setFilterContact } from '../../redux/filterSlice';
+import { getContacts } from '../../redux/selectors';
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return (
-      JSON.parse(window.localStorage.getItem('contacts')) || initialContacts
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+
+  const addContactApp = payload => {
+    let isContactName = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(payload.name.toLowerCase())
     );
-  });
-  const [filter, setFilter] = useState('');
+    let isContactNumber = contacts.filter(contact =>
+      contact.number.toLowerCase().includes(payload.number.toLowerCase())
+    );
+
+    if (isContactName.length) {
+      toast.warn(`Name ${payload.name} is already in your contacts`, {
+        background: '#eebf31',
+        fontSize: '16px',
+        width: '350px',
+      });
+      return;
+    }
+
+    if (isContactNumber.length) {
+      toast.warn(`Number ${payload.number} is already in your contacts`, {
+        background: '#eebf31',
+        fontSize: '16px',
+        width: '350px',
+      });
+      return;
+    }
+
+    dispatch(addContact(payload));
+  };
+
+  const contactDeleteHandler = contactId => {
+    toast.success('Contact is deleted', {
+      fontSize: '16px',
+      width: '350px',
+    });
+    dispatch(deleteContact(contactId));
+  };
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  const formSubmitHandler = data => {
-    const contact = { id: nanoid(), ...data };
-
-    let isContactName = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(data.name.toLowerCase())
-    );
-
-    if (isContactName.length) {
-      toast.warn(`${data.name} is already in contacts`);
-      return;
-    }
-
-    let isContactNumber = contacts.filter(contact =>
-      contact.number.toLowerCase().includes(data.number.toLowerCase())
-    );
-
-    if (isContactNumber.length) {
-      toast.warn(`${data.number} is already in contacts`);
-      return;
-    }
-
-    setContacts(prevState => [...prevState, contact]);
-    toast.success('Contact was added');
-  };
-
-  const handleFilter = value => {
-    setFilter(value);
-  };
-
-  const getContacts = () => {
-    let results = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-    if (!results.length) {
-      toast.warn('Contact wasn`t found');
-    }
-    return results;
-  };
-
-  const deleteContact = contactId => {
-    setContacts(contacts.filter(contact => contact.id !== contactId));
-    toast.success('Contact is deleted');
-  };
-
   return (
-    <section className={css.sectionWrapper}>
-      <h1 className={css.title}>Phonebook</h1>
-      <Section>
-        <ContactForm onSubmit={formSubmitHandler} />
-      </Section>
-      <Section title="Contacts">
-        <Filter filterByName={handleFilter} />
-        {contacts.length ? (
-          <ContactsList contacts={getContacts()} onDelete={deleteContact} />
-        ) : (
-          <span className={cssText.text}>
-            There is no contact in your phonebook. Add your first!
-          </span>
-        )}
-      </Section>
+    <>
+      <section className={css.sectionWrapper}>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm onSubmit={payload => addContactApp(payload)} />
+        <Section title="Contacts"></Section>
+        <Filter filterByName={payload => dispatch(setFilterContact(payload))} />
+        <ContactsList onDelete={payload => contactDeleteHandler(payload)} />
+      </section>
       <ToastContainer />
-    </section>
+    </>
   );
+  // const [contacts, setContacts] = useState(() => {
+  //   return (
+  //     JSON.parse(window.localStorage.getItem('contacts')) || initialContacts
+  //   );
+  // });
+  // const [filter, setFilter] = useState('');
+
+  // useEffect(() => {
+  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
+
+  // const formSubmitHandler = data => {
+  //   const contact = { id: nanoid(), ...data };
+
+  //   let isContactName = contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(data.name.toLowerCase())
+  //   );
+
+  //   if (isContactName.length) {
+  //     toast.warn(`${data.name} is already in contacts`);
+  //     return;
+  //   }
+
+  //   let isContactNumber = contacts.filter(contact =>
+  //     contact.number.toLowerCase().includes(data.number.toLowerCase())
+  //   );
+
+  //   if (isContactNumber.length) {
+  //     toast.warn(`${data.number} is already in contacts`);
+  //     return;
+  //   }
+
+  //   setContacts(prevState => [...prevState, contact]);
+  //   toast.success('Contact was added');
+  // };
+
+  // const handleFilter = value => {
+  //   setFilter(value);
+  // };
+
+  // const getContacts = () => {
+  //   let results = contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase())
+  //   );
+
+  //   if (!results.length) {
+  //     toast.warn('Contact wasn`t found');
+  //   }
+  //   return results;
+  // };
+
+  // const deleteContact = contactId => {
+  //   setContacts(contacts.filter(contact => contact.id !== contactId));
+  //   toast.success('Contact is deleted');
+  // };
+
+  // return (
+  //   <section className={css.sectionWrapper}>
+  //     <h1 className={css.title}>Phonebook</h1>
+  //     <Section>
+  //       <ContactForm onSubmit={formSubmitHandler} />
+  //     </Section>
+  //     <Section title="Contacts">
+  //       <Filter filterByName={handleFilter} />
+  //       {contacts.length ? (
+  //         <ContactsList contacts={getContacts()} onDelete={deleteContact} />
+  //       ) : (
+  //         <span className={cssText.text}>
+  //           There is no contact in your phonebook. Add your first!
+  //         </span>
+  //       )}
+  //     </Section>
+  //     <ToastContainer />
+  //   </section>
+  // );
 };
 
 export default App;
--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,27 +1,45 @@
 import css from './ContactsList.module.css';
 import cssButton from '../ContactForm/ContactForm.module.css';
+import cssText from 'components/ContactsList/ContactsList.module.css';
 
-export default function ContactsList({ contacts, onDelete }) {
+import { useSelector } from 'react-redux';
+import {
+  getContacts,
+  getFilter,
+  getVisibleContacts,
+} from '../../redux/selectors';
+
+export default function ContactsList({ onDelete }) {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const visibleContacts = getVisibleContacts(contacts, filter.filter);
+
   return (
     <ul className={css.contactsListContainer}>
-      {contacts.map(contact => {
-        return (
-          <li className={css.contactsItem} key={contact.id}>
-            <span className={css.text}>
-              {contact.name}: {contact.number}
-            </span>
-            <button
-              className={cssButton.deleteButton}
-              type="button"
-              onClick={() => {
-                onDelete(contact.id);
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        );
-      })}
+      {visibleContacts.length ? (
+        visibleContacts.map(contact => {
+          return (
+            <li className={css.contactsItem} key={contact.id}>
+              <span className={css.text}>
+                {contact.name}: {contact.number}
+              </span>
+              <button
+                className={cssButton.deleteButton}
+                type="button"
+                onClick={() => {
+                  onDelete(contact.id);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })
+      ) : (
+        <span className={cssText.text}>
+          There is no contact in your phonebook. Add your first!
+        </span>
+      )}
     </ul>
   );
 }
