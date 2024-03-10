91aefe22eb8a8863d3fcdc1bff6f7e47e3002bc8
Anna Tsepilova <virgo.tsav@gmail.com>
2024-03-10 12:32:58 +0200

    initial commit

--- src/redux/actions.js
+++
@@ -1,7 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const addContactAction = createAction('contacts/addContact');
-
-export const deleteContactAction = createAction('contacts/deleteContact');
-
-export const setFilterContactAction = createAction('filter/setFilterContact');
